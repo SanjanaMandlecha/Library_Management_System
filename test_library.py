facbef3d3bf343784c53b66a6ad529835f2b6715
SanjanaMandlecha <sanjana.mandlecha6@gmail.com>
2024-09-27 01:18:24 +0530

    Add test for adding books

---
+++ test_library.py
@@ -0,0 +1,13 @@
+import unittest
+
+class Library_Testing(unittest.TestCase):
+    def setUp(self):
+        self.Library = Library()
+
+    def test_add_books(self):
+        book = Book(isbn="978-93-5163-389-1",title="Data Structure Using C",author="Sharad Kumar Verma",publicationYear=2015)
+        self.Library.add_book(book)
+        self.assertIn(book,self.Library.books)
+
+if __name__ == '__main__':
+    unittest.main()
