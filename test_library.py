089eebc78af1faf0dd39670402e5deff37efa3e0
SanjanaMandlecha <sanjana.mandlecha6@gmail.com>
2024-09-27 01:23:59 +0530

    Implement borrow unavailable book feature

--- test_library.py
+++ test_library.py
@@ -16,5 +16,13 @@ class Library_Testing(unittest.TestCase):
         self.Library.borrow_book("978-93-5163-389-1")
         self.assertFalse(book.available)
 
+    def test_borrow_unavailable_book(self):
+        book = Book(isbn="978-93-5163-389-1",title="Data Structure Using C",author="Sharad Kumar Verma",publicationYear=2015)
+        self.Library.add_book(book)
+        self.Library.borrow_book("978-93-5163-389-1")
+        with self.assertRaises(Exception) as message:
+            self.Library.borrow_book("978-93-5163-389-1")
+        self.assertTrue('Book Not Available' in str(message.exception))
+
 if __name__ == '__main__':
     unittest.main()
