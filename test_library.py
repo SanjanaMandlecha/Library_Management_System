e7f7bc61a8cdeec774fc013a5557a389553731c6
SanjanaMandlecha <sanjana.mandlecha6@gmail.com>
2024-09-27 01:27:33 +0530

    Add test for viewing available books

--- test_library.py
+++ test_library.py
@@ -31,5 +31,15 @@ class Library_Testing(unittest.TestCase):
         self.Library.return_book("978-93-5163-389-1")
         self.assertTrue(book.available)
 
+    def test_view_available_books(self):
+        book1 = Book(isbn="978-93-5163-389-1",title="Data Structure Using C",author="Sharad Kumar Verma",publicationYear=2015)
+        book2 = Book(isbn="978-93-5019-561-1", title="Junior Level Books Introduction to Computer", author="Amit Garg", publicationYear=2011)
+        self.Library.add_book(book1)
+        self.Library.add_book(book2)
+        self.Library.borrow_book("978-93-5163-389-1")
+        available_books = self.Library.view_available_books()
+        self.assertIn(book2, available_books)
+        self.assertNotIn(book1, available_books)
+
 if __name__ == '__main__':
     unittest.main()
